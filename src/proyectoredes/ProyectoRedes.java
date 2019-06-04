fdaf5bf0e4ad0e3cbe4fe3edeee0859a25c4ec84
Felipe Mora Chacón <49416627+KayseraCR@users.noreply.github.com>
2019-06-04 15:02:35 -0600

    hola

--- src/proyectoredes/ProyectoRedes.java
+++ src/proyectoredes/ProyectoRedes.java
@@ -18,7 +18,7 @@ public class ProyectoRedes extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FlowController.getInstance().InitializeFlow(stage, null);
-        FlowController.getInstance().goMain();
+        FlowController.getInstance().goMain();//hola
         FlowController.getInstance().goView("PantEmisor");
     }
 
